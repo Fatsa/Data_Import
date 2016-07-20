4de5d628d64a30feabca61cebdc2567b1c3a9487
Fatsa <village@They-call-me-Fatsa.local>
2016-07-20 16:27:52 +0200

    Complete the check of exception

--- CovaRep/CR_Merge.m
+++ CovaRep/CR_Merge.m
@@ -3,12 +3,21 @@ function [ov_Data_CovaRep] = CR_Merge(data_CovaRep)
     ov_Data_CovaRep = zeros(1,74);
     [deepth, length]=size(data_CovaRep);
     
-    sum =0;
-    
     for j = 1: length
+        sum = 0;
+        count = deepth;
         for i =1: deepth
-            sum = sum + data_CovaRep(i,j);
+            a = data_CovaRep(i,j);
+            if ( 0 == isnan(a))
+                sum = sum + a;
+            else
+                count = count -1;
+            end
         end
-        ov_Data_CovaRep(1,j) = sum/deepth;
+        if (0 == count)
+            ov_Data_CovaRep(1,j) = NaN;
+        else
+            ov_Data_CovaRep(1,j) = sum/count;
+        end
     end
 end
