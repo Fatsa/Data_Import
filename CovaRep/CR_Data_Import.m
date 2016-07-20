91810388f46562fcd331aeb653634176491ea22f
Fatsa <village@They-call-me-Fatsa.local>
2016-07-20 10:27:59 +0200

    Added Speech data import function

---
+++ CovaRep/CR_Data_Import.m
@@ -0,0 +1,35 @@
+
+function CR_Data_Import (maindir)
+    % exported file absolute path
+    export_FilePath=uigetdir( 'choose the export folder' );
+
+    % collect all the txt files under the subfolder
+    subdirpath = fullfile( maindir, '*.mat' );
+    % the set of filenames of txt files
+    dat = dir( subdirpath );
+
+    % the set of file names
+    b = length(dat);
+    fileName = cell(b,1);
+    for j = 1 : length( dat )
+        fileName(j)= cellstr(dat(j).name);
+    end    
+
+    % fullfile the absolute path of txt file
+    pathName = fullfile( maindir);
+    export_FileName = strcat(export_FilePath,'/Obadiah');
+    
+    % process each txt file
+    if  ~iscell(fileName)
+        fileName1 = cellstr(fileName);
+        [FR_Lines_temp, lines_Num]=CR_File_Selection(fileName1,pathName);
+    else
+        [FR_Lines_temp, lines_Num]=CR_File_Selection(fileName,pathName);
+    end
+
+    % structurize the final data 
+    %[data_FaceReader]=FR_Synchronization(FR_Lines_temp, lines_Num);
+
+    % export the proessed data into a csv.file
+    %FR_Export_Data(export_FileName,data_FaceReader);
+end
