27e6159a3694554c94b91afcda7fcb674e0ae2f1
Fatsa <village@They-call-me-Fatsa.local>
2016-12-13 16:27:36 +0100

    Segment the text
    delete the number
    delete the unnecessary sentence
    delete the laugh words

--- Main.m
+++ Main.m
@@ -1,7 +1,8 @@
 % the main entrance of Data import which includes the functions selection 
 
 function Main ()
-    maindir = uigetdir('choose the input folder\n');  
+    %maindir = uigetdir('choose the input folder\n'); 
+    maindir = '/Users/village/Desktop/Text/Obadiah';
     LIWC_Data_Import(maindir);
 %    CR_Data_Import(maindir);
 %    universality_Data_Import();
